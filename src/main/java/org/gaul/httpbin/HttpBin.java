42480a8e6d6bef1ba9229d9cbb56fe1b998b3492
Andrew Gaul <andrew@gaul.org>
2019-08-30 21:46:55 -0700

    Update HTTP links to HTTPS

--- src/main/java/org/gaul/httpbin/HttpBin.java
+++ src/main/java/org/gaul/httpbin/HttpBin.java
@@ -6,7 +6,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -26,7 +26,7 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 
 /**
- * Reimplementation of HttpBin http://httpbin.org/ suitable for offline unit
+ * Reimplementation of HttpBin https://httpbin.org/ suitable for offline unit
  * tests.
  */
 public final class HttpBin {
